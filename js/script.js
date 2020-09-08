ecf763b1e62f4fb133aae0dad36d0c7a6fecba51
Pablo <cruzpablomartin@gmail.com>
2020-09-08 00:04:42 -0300

    Game development finished

--- js/script.js
+++ js/script.js
@@ -1,74 +1,108 @@
-// let colors = [
-//    'rgb(224, 86, 253)', 
-//    'rgb(34, 166, 179)', 
-//    'rgb(106, 176, 76)', 
-//    'rgb(240, 147, 43)', 
-//    'rgb(72, 52, 212)', 
-//    'rgb(255, 121, 121)' 
-// ];
-
-
 let body = document.querySelector('body');
 let h1 = document.querySelector('h1');
-let square = document.querySelectorAll('.square');
+let square = document.querySelectorAll('.square'); // Cuadrados
+let mode = document.querySelectorAll('.mode');  // Botones 'Easy' y 'Hard'
 let message = document.querySelector('#message');
 let colorDisplay = document.querySelector('#colorDisplay');
-let button = document.querySelector('#reset');
-let easy = document.querySelector('#easy');
-let hard = document.querySelector('#hard');
+let resetColorsButton = document.querySelector('#reset'); // Boton 'New Colors'
+
+let colors, pickedColor, numberOfSquares;
+
+// Cantidad de cuadrados iniciales. 
+numberOfSquares = 6;
+
+init();
 
 
-// Cantidad de cuadrados que estan en juego. 
-let numberOfSquares = 6;
-   
-// Generar un arreglo de 6 colores.
-let colors = generateRandomColors(6);
-
-// Generar al azar el color ganador.
-let pickedColor = colors[pickColor()];
-
-// Mostrar #colorDisplay el color ganador. 
-colorDisplay.innerHTML = pickedColor;
-
-
-
-// Agregar colores a los cuadrados.
-for (let i = 0; i < square.length; i++) {
-   square[i].style.backgroundColor = colors[i];
+function init () {
+   addEventModes();
+   addEventSquares();
+   reset();
 }
 
 
-// Agregrar eventos a los cuadrados         
-for (let i = 0; i < square.length; i++) {
-   
-   square[i].addEventListener('click', (e) => { 
-   
-      let clickedColor = square[i].style.backgroundColor; 
-      
-      // Si adivina el color ganador: click sobre el cuadrado que almacena el color de pickedColor
-      if (clickedColor === pickedColor) {
-         // 1). Colocar mensaje 'Correct!' a #message
-         message.innerHTML = 'Correct!';
-         // 2). Establecer el color de fondo de h1 con el color ganador.
-         h1.style.backgroundColor = pickedColor;
-         // 3). Establecer todos los cuadrados con el color ganador.
-         changeColors(pickedColor);
-         // 4). Cambiar mensaje del boton a 'Play Again?'
-         button.innerHTML = 'Play Again?';
-      } else {         
+// Agrega eventos a los botones 'Easy' y 'Hard'.
+function addEventModes () {
+   for (let i = 0; i < mode.length; i++) {
 
-         // 1). Establecer al cuadrado que se le hizo click el color de fondo del body.
-         square[i].style.backgroundColor = body.style.backgroundColor;
-         // 2). Colocar mensaje 'Try Again' a #message
-         message.innerHTML = 'Try Again';
-         // Agregar efecto fadeOut al ocultar el cuadrado.  
-         square[i].classList.add('fadeOut');
+      mode[i].addEventListener('click', (e) => {  // 0 === Easy   1 === Hard
+
+         mode[0].classList.remove('selected');
+         mode[1].classList.remove('selected');
+         mode[i].classList.add('selected');
+
+         numberOfSquares = (mode[i].innerHTML === 'Easy') ? 3 : 6;
+         
+         reset();
+      });
+   }
+}
+
+
+// Agregra eventos a los cuadrados.         
+function addEventSquares () {
+
+   for (let i = 0; i < square.length; i++) {
+
+      square[i].addEventListener('click', (e) => {
+         let clickedColor = square[i].style.backgroundColor;
+
+         // Si adivina el color ganador: click sobre el cuadrado que almacena el color de pickedColor
+         if (clickedColor === pickedColor) {
+            // 1). Colocar mensaje 'Correct!' a #message
+            message.innerHTML = 'Correct!';
+            // 2). Establecer el color de fondo de h1 con el color ganador.
+            h1.style.backgroundColor = pickedColor;
+            // 3). Establecer todos los cuadrados con el color ganador.
+            changeColors(pickedColor);
+            // 4). Cambiar mensaje del boton a 'Play Again?'
+            resetColorsButton.innerHTML = 'Play Again?';
+         } else {
+            // 1). Establecer al cuadrado que se le hizo click el color de fondo del body.
+            square[i].style.backgroundColor = body.style.backgroundColor;
+            // 2). Colocar mensaje 'Try Again' a #message
+            message.innerHTML = 'Try Again';
+            // Agregar efecto fadeOut al ocultar el cuadrado.  
+            square[i].classList.add('fadeOut');
+         }
+      });
+   }
+}
+
+
+// Agregar evento al boton 'New Colors'.
+resetColorsButton.addEventListener('click', (e) => {
+   reset();
+});
+
+
+// Reestablece la cantidad de cuadrados visibles y les agrega los colores según el modo 'Easy' o 'Hard'. 
+function reset () {
+   // Generar arreglo de colores ( numberOfSquares: 3 || 6 )
+   colors = generateRandomColors(numberOfSquares);
+   // Generar al azar el color ganador.
+   pickedColor = colors[pickColor()];
+   // Mostrar en #colorDisplay el color ganador. 
+   colorDisplay.innerHTML = pickedColor;
+
+   // Hacer visible numberOfSquares cantidad de cuadrados y establecerle los colores.
+   for (let i = 0; i < square.length; i++) {
+      if (colors[i]) {  // Si existe un elemento, retorna string === true . Sino retorna undefined === false
+         square[i].style.backgroundColor = colors[i];
+         square[i].style.display = 'block';
+      } else {
+         square[i].style.display = 'none';
       }
-   });
+   }
+
+   // Resetear el mensaje del boton a 'New Colors'.
+   resetColorsButton.innerHTML = 'New Colors';
+   // Eliminar posible mensaje que se haya generado de 'Correct' o 'Try Again'.
+   message.innerHTML = '';
+   // Reestablecer el color de fondo original de h1.
+   h1.style.backgroundColor = '#4682B4';
 }
 
-
-// FUNCIONES
 
 // Establece el mismo color a todos los cuadrados.
 function changeColors (color) {
@@ -77,18 +111,15 @@ function changeColors (color) {
    }
 }
 
-
 // Retorna un número entero aleatorio entre 0 y colors.length - 1 (inclusive). 
-function pickColor() {
+function pickColor () {
    return Math.round(Math.random() * (colors.length - 1));  
 }
-
 
 // Retorna un color aleatorio (string)   // 'rgb(n1,n2,n3)'    // entre 0 y 255 (inclusive)
 function randomColor () {
    return 'rgb('+ Math.round(Math.random() * 255) +', '+ Math.round(Math.random() * 255) +', '+ Math.round(Math.random() * 255)+')';
 }
-
 
 // Retorna un arreglo de n colores aleatorios.
 function generateRandomColors (n) {
@@ -98,78 +129,3 @@ function generateRandomColors (n) {
    }
    return arrColors;
 }
-
-
-// EVENTOS
-
-button.addEventListener('click', (e) => {
-   
-   // Generar nuevos colores en el arreglo ( numberOfSquares === 3 || 6 )
-   colors = generateRandomColors(numberOfSquares);
-   // Generar al azar el color ganador.
-   pickedColor = colors[pickColor()];
-   // Mostrar en #colorDisplay el color ganador. 
-   colorDisplay.innerHTML = pickedColor;
-
-   // Reiniciar los colores
-   for (let i = 0; i < colors.length; i++) {
-      square[i].style.backgroundColor = colors[i];
-   }
-
-   // Resetear el mensaje del boton a 'New Colors'
-   button.innerHTML = 'New Colors';
-
-   // Eliminar posible mensaje que se haya generado de 'Correct' o 'Try Again'
-   message.innerHTML = '';
-
-   // Reestablecer el color de fondo original de h1.
-   h1.style.backgroundColor = '#4682B4';
-});
-
-
-easy.addEventListener('click', (e) => {
-   easy.classList.add('selected');
-   hard.classList.remove('selected');
-
-   // Generar un arreglo de 3 colores
-   colors = generateRandomColors(3);
-   // Generar al azar el color ganador.
-   pickedColor = colors[pickColor()];
-   // Mostrar en #colorDisplay el color ganador. 
-   colorDisplay.innerHTML = pickedColor;
-
-   // Establecer los colores a los 3 primeros cuadrados y ocultar Los 3 cuadrados restantes.  
-   for (let i = 0; i < square.length; i++) {
-      if (colors[i]) {  // Si existe un elemento, retorna string === true . Sino retorna undefined === false
-         square[i].style.backgroundColor = colors[i];
-      } else {
-         square[i].style.display = 'none';
-      }
-   }
-
-   // Reestablecer la cantidad de cuadrados en juego. 
-   numberOfSquares = 3;
-});
-
-
-hard.addEventListener('click', (e) => {
-   hard.classList.add('selected');
-   easy.classList.remove('selected');
-
-   // Generar un arreglo de 6 colores
-   colors = generateRandomColors(6);
-   // Generar al azar el color ganador.
-   pickedColor = colors[pickColor()];
-   // Mostrar en #colorDisplay el color ganador. 
-   colorDisplay.innerHTML = pickedColor;
-
-
-   // Hacer visible los 6 cuadrados (por si el usuario cambio al menos una vez a modo fácil) y agregar los colores a los cuadrados.
-   for (let i = 0; i < square.length; i++) {
-      square[i].style.display = 'block';
-      square[i].style.backgroundColor = colors[i];
-   }
-   
-   // Reestablecer la cantidad de cuadrados en juego. 
-   numberOfSquares = 6;
-});
